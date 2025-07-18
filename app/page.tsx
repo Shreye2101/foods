41ab69236ed3e0efbb20393670f087ae3519aaf6
shreyash <shreye2101@gmail.com>
2025-07-19 02:23:15 +0530

    manual changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import { HeroSection } from "@/components/sections/hero-section"
 import { CategoriesSection } from "@/components/sections/categories-section"
 import { FeaturesSection } from "@/components/sections/features-section"
 import { CTASection } from "@/components/sections/cta-section"
-<<<<<<< HEAD
+
 import TrustedCustomers from "../components/Trusted-customer"
-=======
->>>>>>> 9c3348b3824ff010b509339529441205d7b91d2d
+
 
 export default function HomePage() {
   return (
@@ -14,13 +13,12 @@ export default function HomePage() {
       <HeroSection />
       <CategoriesSection />
       <FeaturesSection />
-<<<<<<< HEAD
+
       <TrustedCustomers/>
       <CTASection />
 
-=======
+
       <CTASection />
->>>>>>> 9c3348b3824ff010b509339529441205d7b91d2d
     </MainLayout>
   )
 }
